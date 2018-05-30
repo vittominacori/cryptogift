d3a7ccdd86531567a4f81f8723a67c2eca63939c
vittominacori <vittoriominacori@gmail.com>
2018-05-30 22:46:30 +0200

    updated solcover.js

--- .solcover.js
+++ .solcover.js
@@ -1,9 +1,7 @@
 module.exports = {
-    norpc: true,
-    testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-    compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-    skipFiles: [
-        'lifecycle/Migrations.sol',
-        'mocks'
-    ]
-}
+  copyNodeModules: true,
+  norpc: true,
+  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
+  compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
+  skipFiles: []
+};
