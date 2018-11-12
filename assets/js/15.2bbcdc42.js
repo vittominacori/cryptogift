a2b71a8211cf0f00189bc0aa32fec75746953698
vittominacori <vittoriominacori@gmail.com>
2018-11-12 23:51:21 +0100

    publish: adding privacy and terms
    generated from commit d5fcad3df3f9a7030871bc3ba64c38ddbb2f6fde

---
+++ assets/js/15.2bbcdc42.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{200:function(t,n,e){"use strict";e.r(n);var i=e(4),s=Object(i.a)({},function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"content"})},[],!1,null,null,null);s.options.__file="privacy-policy.md";n.default=s.exports}}]);
