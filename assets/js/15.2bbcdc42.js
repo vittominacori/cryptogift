477422c7270a3c2be2ac819d8f94436fe70e3384
vittominacori <vittoriominacori@gmail.com>
2018-11-14 22:42:20 +0100

    publish: adding sitemap
    generated from commit 6d2128663faab61f43efb512d5812407cec2582f

--- assets/js/15.2bbcdc42.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{200:function(t,n,e){"use strict";e.r(n);var i=e(4),s=Object(i.a)({},function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"content"})},[],!1,null,null,null);s.options.__file="privacy-policy.md";n.default=s.exports}}]);
--- assets/js/7.a27cf865.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{192:function(n,w,o){}}]);
