16791e4859e90b1521297403e9ff8ac5d37c09c1
vittominacori <vittoriominacori@gmail.com>
2018-11-14 22:05:53 +0100

    changes in footer

--- .vuepress/theme/Footer.vue
+++ .vuepress/theme/Footer.vue
@@ -1,13 +1,13 @@
 <template>
-    <b-row class="d-print-none">
+    <b-row class="d-print-none mt-5">
         <b-col lg="10" offset-lg="1" class="text-center text-white p-0">
             <ul class="list-inline mt-2">
                 <li class="list-inline-item">
-                    <b-link class="text-light" to="/terms-of-use.html"><small>Terms of use</small></b-link>
+                    <b-link class="text-light" target="_blank" to="/terms-of-use.html"><small>Terms of use</small></b-link>
                 </li>
                 <li class="list-inline-item">|</li>
                 <li class="list-inline-item">
-                    <b-link class="text-light" to="/privacy-policy.html"><small>Privacy policy</small></b-link>
+                    <b-link class="text-light" target="_blank" to="/privacy-policy.html"><small>Privacy policy</small></b-link>
                 </li>
                 <li class="list-inline-item">|</li>
                 <li class="list-inline-item"><small><b-link class="text-light" href="https://github.com/vittominacori" target="_blank">@vittominacori</b-link> &copy; 2018</small>
