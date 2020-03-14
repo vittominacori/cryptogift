633f691c4f460fee63e4eed1ef8c87ce8e236601
vittominacori <vittoriominacori@gmail.com>
2020-03-14 17:38:49 +0100

    publish: add latest donations on coronavirus page
    generated from commit 8b7d444223643cae32e81c07fee16243d334a32b

--- assets/js/20.b0280c3f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{195:function(n,w,o){}}]);
---
+++ assets/js/23.2ae33234.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{196:function(n,w,o){}}]);
